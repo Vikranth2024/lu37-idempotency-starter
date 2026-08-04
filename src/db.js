c3dad7c9bcff588f91dafd36e8266813c4c73b6f
Vikranth2024 <vikranthsabari436@gmail.com>
2026-08-04 14:55:50 +0530

    Use IncidentHub assignment scenario

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
 const pgp = require('pg-promise')();
-const connection = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:54329/fireline';
+const connection = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:54329/incidenthub';
 const db = pgp(connection);
 module.exports = { db, pgp };
